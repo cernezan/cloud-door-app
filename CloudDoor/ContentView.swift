368cb47ffdf551ebedd9c8492e51dcde9e825561
Dejan Levec <dejan@dejanlevec.com>
2025-01-15 21:57:22 +0100

    contentView: show message if no credentials

--- CloudDoor/ContentView.swift
+++ CloudDoor/ContentView.swift
@@ -23,12 +23,6 @@ struct ContentView: View {
         // When starting the application, load locations from cache, to avoid waiting for the initial response from the API.
         let cachedLocations = cache.getCachedLocations() ?? []
         self.locations = getLocationsWithDistance(locations: cachedLocations, distanceToLocation: nil)
-        
-        if configuration.get().hostname.isEmpty {
-            alertTitle = "Error"
-            alertMessage = "Please set the hostname in the configuration."
-            showAlert = true
-        }
     }
     
     func refresh() {
@@ -49,19 +43,24 @@ struct ContentView: View {
     }
 
     var body: some View {
-        VStack {
-            List(locations) { index in
-                HStack {
-                    Text("\(index.location.name) \(optionalDistanceToString(distance: index.distance))")
-                    Spacer()
-                }
-                .contentShape(Rectangle())
-                .onReceive(locationManager.$location) { value in
-                    let rawLocations = self.locations.map { $0.location }
-                    self.locations = getLocationsWithDistance(locations: rawLocations, distanceToLocation: value)
-                }
-                .onTapGesture {
-                    if index.inRadius {
+        ZStack {
+            VStack {
+                Label("Please configure credentials in Settings.", systemImage: "info.circle")
+            }
+            .opacity(configuration.get().hostname.isEmpty ? 1: 0)
+            VStack {
+                List(locations) { index in
+                    HStack {
+                        Text("\(index.location.name) \(optionalDistanceToString(distance: index.distance))")
+                        Spacer()
+                    }
+                    .contentShape(Rectangle())
+                    .onReceive(locationManager.$location) { value in
+                        let rawLocations = self.locations.map { $0.location }
+                        self.locations = getLocationsWithDistance(locations: rawLocations, distanceToLocation: value)
+                    }
+                    .onTapGesture {
+                        if index.inRadius {
                             Task {
                                 do {
                                     let api = API.initFromConfiguration(configuration: Configuration())
@@ -77,33 +76,35 @@ struct ContentView: View {
                                     showAlert = true
                                 }
                             }
-                    } else {
-                        alertTitle = "Error"
-                        
-                        if let distance = index.distance {
-                            alertMessage = "Door '\(index.location.name)' too far away (\(distance)m > \(index.location.geolocations[0].radius)m)"
                         } else {
-                            alertMessage = "Cannot open door, since location of the device is not known. Please check device settings, to ensure application has location permissions."
+                            alertTitle = "Error"
+                            
+                            if let distance = index.distance {
+                                alertMessage = "Door '\(index.location.name)' too far away (\(distance)m > \(index.location.geolocations[0].radius)m)"
+                            } else {
+                                alertMessage = "Cannot open door, since location of the device is not known. Please check device settings, to ensure application has location permissions."
+                            }
+                            showAlert = true
                         }
-                        showAlert = true
                     }
                 }
+                if let placemark = locationManager.placemark {
+                    Text("Location: \(placemark.subThoroughfare ?? "") \(placemark.thoroughfare ?? ""), \(placemark.locality ?? "")")
+                } else {
+                    Text("Location: unknown")
+                }
             }
-            if let placemark = locationManager.placemark {
-                Text("Location: \(placemark.subThoroughfare ?? "") \(placemark.thoroughfare ?? ""), \(placemark.locality ?? "")")
-            } else {
-                Text("Location: unknown")
+            .padding()
+            .onAppear {
+                if configuration.get().username != "" {
+                    refresh()
+                }
             }
+            .alert(isPresented: $showAlert, content: {
+                Alert(title: Text(self.alertTitle), message: Text(self.alertMessage), dismissButton: .default(Text("OK")))
+            })
+            .opacity(configuration.get().hostname.isEmpty ? 0: 1)
         }
-        .padding()
-        .onAppear {
-            if configuration.get().username != "" {
-                refresh()
-            }
-        }
-        .alert(isPresented: $showAlert, content: {
-            Alert(title: Text(self.alertTitle), message: Text(self.alertMessage), dismissButton: .default(Text("OK")))
-        })
     }
 }
 
